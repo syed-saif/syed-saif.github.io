dae355ab604316ad842bd8c883df832766f1b150
Syed Saif <sydsaifullah7@gmail.com>
2021-09-22 22:26:04 +0530

    copy-pasted the contents of the src folder, from Chetan's template

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,28 +1,24 @@
-import * as React from "react"
-import { Link } from "gatsby"
-import { StaticImage } from "gatsby-plugin-image"
+import React from "react"
 
 import Layout from "../components/layout"
-import Seo from "../components/seo"
+import SEO from "../components/seo"
+// Components
+import Header from "../components/Header"
+import Work from "../components/Work"
+import About from "../components/about"
+import Skills from "../components/skills"
+import Promotion from "../components/Promotion"
+import Footer from "../components/Footer"
 
 const IndexPage = () => (
   <Layout>
-    <Seo title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <StaticImage
-      src="../images/gatsby-astronaut.png"
-      width={300}
-      quality={95}
-      formats={["auto", "webp", "avif"]}
-      alt="A Gatsby astronaut"
-      style={{ marginBottom: `1.45rem` }}
-    />
-    <p>
-      <Link to="/page-2/">Go to page 2</Link> <br />
-      <Link to="/using-typescript/">Go to "Using TypeScript"</Link>
-    </p>
+    <SEO title="Portfolio Template" />
+    <Header></Header>
+    <Work></Work>
+    <About></About>
+    <Skills></Skills>
+    <Promotion></Promotion>
+    <Footer></Footer>
   </Layout>
 )
 
